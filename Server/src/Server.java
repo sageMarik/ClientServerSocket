a32f2148f51aa1d0c660f179d973fbc5ffd94300
sageMarik <marat3014@gmail.com>
2023-10-18 17:25:16 +0300

    update

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,32 +1,29 @@
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        try(ServerSocket server = new ServerSocket(8000))
-        {
+        try (ServerSocket server = new ServerSocket(8000)) {
             System.out.println("Server started!");
 
-            Socket socket = server.accept();
-            System.out.println("Client connected");
-
-            BufferedWriter writer =
-                    new BufferedWriter(
-                            new OutputStreamWriter(
-                                    socket.getOutputStream()));
-            writer.write("HELLO FROM SERVER");
-            writer.newLine();
-            writer.flush();
-
-            writer.close();
-            socket.close();
+            try (
+                    Socket socket = server.accept();
+                    BufferedWriter writer =
+                            new BufferedWriter(
+                                    new OutputStreamWriter(
+                                            socket.getOutputStream())))
+            {
+                writer.write("HELLO FROM SERVER");
+                writer.newLine();
+                writer.flush();
+            }
 
 
-        } catch (IOException e){
-           throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-
     }
 }
